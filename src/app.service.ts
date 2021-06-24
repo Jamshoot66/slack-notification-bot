3cdc739fbc7707842872488c1387f1c90fa627a4
Sergey Gorshkov <sgorshkov@voximplant.com>
2021-06-24 11:52:48 +0300

    feat: parse message

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export interface IReleaseCommitData {
 
 const checkCommit = (commitTitle: string): IReleaseCommitData => ({
   isRelease: Boolean(commitTitle.match(/(release.*:)/g)?.length),
-  version: commitTitle.match(/(v\d.\d.\d)/g)?.[0] ?? '',
+  version: commitTitle.match(/(v?\d.\d.\d)/g)?.[0] ?? '',
 });
 
 interface ISlackMessageField {
@@ -53,8 +53,73 @@ export class AppService {
     return this.httpService.post(url, slackMessage).toPromise();
   }
 
+  parseMessage(msg: string): string[] {
+    return msg
+      .split('\n')
+      .filter((line) => line.length)
+      .map((line) => {
+        // prepare bold
+        let newLine = line;
+        const boldRegex = new RegExp(/\*\*(.+)\*\*/, 'gm');
+        const boldMatches = [...newLine.matchAll(boldRegex)].flat();
+        console.log('boldMatches ', boldMatches);
+
+        if (boldMatches[1]) {
+          newLine = newLine
+            .replace('*', '')
+            .replace(boldMatches[0], `*${boldMatches[1]}*`);
+        }
+
+        // prepare links
+        const linkRegex = new RegExp(/\((\[.+\])(\(https?.+\))\)/, 'gm');
+        const linkMatches = [...newLine.matchAll(linkRegex)].flat();
+        if (linkMatches[2]) {
+          newLine = newLine
+            .replace(linkMatches[0], `<${linkMatches[2]}|${linkMatches[1]}}>`)
+            .replace('(', '')
+            .replace('}', '');
+        }
+
+        // prepare header
+        const headerRegex = new RegExp(/### (.+)/, 'gm');
+        const headerMatches = [...newLine.matchAll(headerRegex)].flat();
+        if (headerMatches[1]) {
+          newLine = newLine.replace(
+            headerMatches[0],
+            `\n *${headerMatches[1]}* \n`,
+          );
+        }
+
+        // prepare diff
+        const diffRegex = new RegExp(
+          /(# \[.+\])(\(https:\/\/[\w.,\/\-\)]+)/,
+          'gm',
+        );
+        const diffMatches = [...newLine.matchAll(diffRegex)].flat();
+        if (diffMatches[2]) {
+          const link = diffMatches[2].replace(/[()]/g, '');
+          newLine = `<${link}|Diff with previous version>`;
+        }
+
+        // clean misk
+        const headerDuplicateRegex = new RegExp(
+          /release: \d\.\d\.\d \[skip ci\]/,
+          'gm',
+        );
+        const headerDuplicateMatches = [
+          ...newLine.matchAll(headerDuplicateRegex),
+        ].flat();
+        if (headerDuplicateMatches?.length) {
+          newLine = '';
+        }
+
+        return newLine;
+      });
+  }
+
   async parseWebhook(props): Promise<void> {
     const changelogUrl = this.configService.get<string>('CHANGELOG_URL');
+
     for await (const commit of props.commits) {
       const { isRelease, version } = checkCommit(commit.title);
       if (isRelease) {
@@ -78,7 +143,7 @@ export class AppService {
               type: 'section',
               text: {
                 type: 'mrkdwn',
-                text: commit.message,
+                text: this.parseMessage(commit.message).join('\n'),
               },
               accessory: {
                 type: 'image',
